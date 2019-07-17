0f0fae0be9ad4fc3131f70042d6a6f8ce4d3e3b5
Tyler Lam <tylerlam888@gmail.com>
2019-07-16 23:11:40 -0400

    changed distances to manhattan distance

--- station-distances.py
+++ station-distances.py
@@ -8,6 +8,18 @@ sd = pd.read_csv("data/station-data.csv")
 sd_rows = sd.shape[0]
 sd_distances = {}
 
+def manhattanDistance(coords_1, coords_2):
+    x1, y1 = coords_1
+    x2, y2 = coords_2
+
+    # keep ycoord constant
+    xdistance = geodesic((x1, y1), (x2, y1)).m
+    # keep xcoord constant
+    ydistance = geodesic((x1, y1), (x1, y2)).m
+
+    return xdistance + ydistance
+
+
 #Parsing through dataframe and getting distances between each Terminal
 for x in range(0,sd_rows):
     for y in range(0,sd_rows):
@@ -15,7 +27,7 @@ for x in range(0,sd_rows):
         coords_1 = (sd.loc[x,'Latitude'], sd.loc[x,'Longitude'])
         coords_2 = (sd.loc[y,'Latitude'], sd.loc[y,'Longitude'])
 
-        distance = geodesic(coords_1, coords_2).m
+        distance = manhattanDistance(coords_1, coords_2)
 
         #Appending to dictionary
         key = sd.loc[x,'Terminal']
@@ -23,5 +35,6 @@ for x in range(0,sd_rows):
 
 #Putting distances into dataframe
 sdf = pd.DataFrame.from_dict(sd_distances)
+print(sdf)
 #sdf.to_csv('station-distances.csv', index =True, header=True) #Optional if you want the distances in CSV form
 
