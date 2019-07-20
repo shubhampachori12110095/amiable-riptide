79e25b92d3dd701c885698700416083495cc394e
Matthieu Court <matthieu.court@gmail.com>
2019-07-20 02:36:11 -0400

    a lil bit of shit

--- test.py
+++ test.py
@@ -1,55 +1,90 @@
 from __future__ import print_function
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
-import pandas as pd
-import random
 
-# importData = pd.read_csv('station-distances.csv')
-# print (importData)
 
-def create_model():
-    
+def create_data_model():
+    """Stores the data for the problem."""
     data = {}
-    # Grab the distance and demand data
-    importData = pd.read_csv('station-distances.csv', index_col=None)
-    importData = importData.astype(int)
-    print(importData)
-    importDataAux = importData.drop(importData.columns[0], axis=1)
-    cleanData = importDataAux.values.tolist()
-    # print (cleanData)
-
-    # Create random demands for the moments.
-    randomDemands = []
-    numberOfStations = 81
-    for i in range(numberOfStations):
-        randomDemands.append(random.randint(3, 15))
-
-    # Hardcode the vehicle capacity for the moment.
-    vehicleCapacity = 15
-    # Find the total demand that will have to be distributed.
-    totalDemand = sum(randomDemands)
-    # Create the vehicle capacities array...
-        # But first, excuse the funky way of doing ceiling divisions in Python.
-    numberOfVehicles = ((totalDemand - 1) // vehicleCapacity) + 1
-    vehicleCapacityList = [vehicleCapacity] * numberOfVehicles
-
-    # The distance_matrix is a list of lists.
-    data['distance_matrix'] = cleanData
-    data['demands'] = randomDemands
-    print(cleanData)
-    print(len(cleanData))
-    print(len(randomDemands))
-    print(vehicleCapacity)
-    print(len(vehicleCapacityList))
-    data['vehicle_capacities'] = vehicleCapacityList
-    data['num_vehicles'] = numberOfVehicles
+    data['distance_matrix'] = [
+        [
+            0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354,
+            468, 776, 662
+        ],
+        [
+            548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674,
+            1016, 868, 1210
+        ],
+        [
+            776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164,
+            1130, 788, 1552, 754
+        ],
+        [
+            696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822,
+            1164, 560, 1358
+        ],
+        [
+            582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708,
+            1050, 674, 1244
+        ],
+        [
+            274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628,
+            514, 1050, 708
+        ],
+        [
+            502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856,
+            514, 1278, 480
+        ],
+        [
+            194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320,
+            662, 742, 856
+        ],
+        [
+            308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662,
+            320, 1084, 514
+        ],
+        [
+            194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388,
+            274, 810, 468
+        ],
+        [
+            536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764,
+            730, 388, 1152, 354
+        ],
+        [
+            502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114,
+            308, 650, 274, 844
+        ],
+        [
+            388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194,
+            536, 388, 730
+        ],
+        [
+            354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0,
+            342, 422, 536
+        ],
+        [
+            468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536,
+            342, 0, 764, 194
+        ],
+        [
+            776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274,
+            388, 422, 764, 0, 798
+        ],
+        [
+            662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730,
+            536, 194, 798, 0
+        ],
+    ]
+    data['demands'] = [0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]
+    data['vehicle_capacities'] = [15, 15, 15, 15, 15, 15]
+    data['num_vehicles'] = 6
     data['depot'] = 0
-    print(randomDemands)
     return data
+
 
 def print_solution(data, manager, routing, assignment):
     """Prints assignment on console."""
-    print('made it')
     total_distance = 0
     total_load = 0
     for vehicle_id in range(data['num_vehicles']):
@@ -79,7 +114,7 @@ def print_solution(data, manager, routing, assignment):
 def main():
     """Solve the CVRP problem."""
     # Instantiate the data problem.
-    data = create_model()
+    data = create_data_model()
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
@@ -88,7 +123,6 @@ def main():
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
-    print('#1 ****')
 
     # Create and register a transit callback.
     def distance_callback(from_index, to_index):
@@ -120,7 +154,6 @@ def main():
         True,  # start cumul to zero
         'Capacity')
 
-    print('#2 ****')
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
@@ -132,6 +165,7 @@ def main():
     # Print solution on console.
     if assignment:
         print_solution(data, manager, routing, assignment)
+
 
 if __name__ == '__main__':
     main()
